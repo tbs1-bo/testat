0756ead0bed4fb21f11e9096f84022907b21993d
Marco Bakera <marco@bakera.de>
2022-02-19 23:59:50 +0100

    doc

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
+# default config values. copy this to e.g. config.local.py and point
+# env var TESTAT_CONF to the new file to overwrite defaults.
+
 # can be generated with: python -c 'import secrets; print(secrets.token_hex())'
 SECRET_KEY = 'change this'
 
 FLASK_APP = 'testate'
 FLASK_ENV = 'development'
 
+# Domain of the app - can be empty. Should be used in contexts of running the 
+# app behind a proxy. In this case place the hostname of main server in this
+# variable.
 APP_DOMAIN = ''
 
+# User will be authorised against this SMTP-server.
 SMTP_AUTHSERVER = 'smtp.example.com'
 
+# Configure the database uri that should be used to connect to the database.
 import os
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.environ["PWD"]}/testate.db'
 # defaults to false in future release (2022-02-13)
