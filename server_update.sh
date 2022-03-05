12c0a855496009010ade5ea019df78c3f1135766
Marco Bakera <marco@bakera.de>
2022-03-05 17:25:07 +0100

    update deps

--- server_update.sh
+++ server_update.sh
@@ -5,5 +5,8 @@
 echo "update repo at server $DEPLOY_SERVER"
 ssh $DEPLOY_SERVER "git -C $DEPLOY_DIR pull"
 
+echo "update deps"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update"
+
 echo "restart service"
 ssh -t main.tbs1.de 'sudo supervisorctl restart testat'
