9d9459c7620595e9a13d7f53167de1ceb133a5ca
Marco Bakera <marco@bakera.de>
2022-03-28 18:34:44 +0200

    deployment: dont update dev deps

--- server_update.sh
+++ server_update.sh
@@ -6,7 +6,7 @@ echo "update repo at server $DEPLOY_SERVER"
 ssh $DEPLOY_SERVER "git -C $DEPLOY_DIR pull"
 
 echo "update deps"
-ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update --no-dev"
 
 echo "restart service"
 ssh -t main.tbs1.de 'sudo supervisorctl restart testat'
