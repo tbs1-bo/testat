f28a50239c2ccb77bcd6429e7fe30df5005e39ba
Marco Bakera <marco@bakera.de>
2022-04-16 08:40:41 +0200

    list outdated deps

--- server_update.sh
+++ server_update.sh
@@ -5,7 +5,10 @@
 echo "update repo at server $DEPLOY_SERVER"
 ssh $DEPLOY_SERVER "git -C $DEPLOY_DIR pull"
 
-echo "update deps"
+echo "outdated deps:"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry show --outdated"
+
+echo "update deps:"
 ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update --no-dev"
 
 echo "restart service"
