a69f5f6045d65e11bac95432fe27ede7ee9f7ae0
Marco Bakera <marco@bakera.de>
2022-04-17 16:59:09 +0200

    ignore outdated dev deps

--- server_update.sh
+++ server_update.sh
@@ -6,7 +6,7 @@ echo "update repo at server $DEPLOY_SERVER"
 ssh $DEPLOY_SERVER "git -C $DEPLOY_DIR pull"
 
 echo "outdated deps:"
-ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry show --outdated"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry show --no-dev --outdated"
 
 echo "update deps:"
 ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update --no-dev"
