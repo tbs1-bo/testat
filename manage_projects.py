461fc601704e5d67092b44504eaa743441fc614c
Marco Bakera <marco@bakera.de>
2022-12-20 20:40:24 +0100

    func extracted

--- manage_projects.py
+++ manage_projects.py
@@ -1,37 +1,41 @@
 from testate import Card, app
 import sys
 
+def run(cmd):
+    if cmd == 'ls':
+        print('project name \t| #milestones \t| visible')
+        for card in Card.query.group_by(Card.project_name):
+            print(f'{card.project_name} \t| {len(card.milestones)} \t| {card.is_visible}')
+        
+    elif cmd == 'rm':
+        pname = input('Projekt? ')
+        for card in Card.query.filter(Card.project_name == pname):
+            print(f'delete card {card.id} of {card.student_name}')
+            card.delete()
+
+    elif cmd == 'hide':
+        pname = input('Projekt? ')
+        for card in Card.query.filter(Card.project_name == pname):
+            print(f'hiding card of {card.student_name}')
+            card.visibility(False)
+
+    elif cmd == 'show':
+        pname = input('Projekt? ')
+        for card in Card.query.filter(Card.project_name == pname):
+            print(f'unhiding card of {card.student_name}')
+            card.visibility(True)
+
+    else:
+        print(f'unsupported command')
+
 def main():
     if len(sys.argv) != 2:
         print("ls, rm, hide or show?")
         return
 
     with app.app_context():
-        if sys.argv[1] == 'ls':
-            print('project name \t| #milestones \t| visible')
-            for card in Card.query.group_by(Card.project_name):
-                print(f'{card.project_name} \t| {len(card.milestones)} \t| {card.is_visible}')
-            
-        elif sys.argv[1] == 'rm':
-            pname = input('Projekt? ')
-            for card in Card.query.filter(Card.project_name == pname):
-                print(f'delete card {card.id} of {card.student_name}')
-                card.delete()
+        run(sys.argv[1])
 
-        elif sys.argv[1] == 'hide':
-            pname = input('Projekt? ')
-            for card in Card.query.filter(Card.project_name == pname):
-                print(f'hiding card of {card.student_name}')
-                card.visibility(False)
-
-        elif sys.argv[1] == 'show':
-            pname = input('Projekt? ')
-            for card in Card.query.filter(Card.project_name == pname):
-                print(f'unhiding card of {card.student_name}')
-                card.visibility(True)
-
-        else:
-            print(f'unsupported command')
 
 if __name__ == '__main__':
     main()
--- manage_user.py
+++ manage_user.py
@@ -2,29 +2,32 @@ from cgi import test
 import testate
 import sys
 
+def run(cmd):
+    if cmd == 'add':
+        uid = input('Username? ')
+        dbu = testate.DBUser(uid=uid)
+        testate.db.session.add(dbu)
+        testate.db.session.commit()
+
+    elif cmd == 'ls':
+        for user in testate.User.all():
+            print(user)
+        
+    elif cmd == 'rm':
+        uid = input('Username? ')
+        u = testate.User(uid)
+        u.delete()
+
+    else:
+        print(f'unsupported command')
+
 def main():
     if len(sys.argv) != 2:
         print("add, ls or rm?")
         return
 
     with testate.app.app_context():
-        if sys.argv[1] == 'add':
-            uid = input('Username? ')
-            dbu = testate.DBUser(uid=uid)
-            testate.db.session.add(dbu)
-            testate.db.session.commit()
-
-        elif sys.argv[1] == 'ls':
-            for user in testate.User.all():
-                print(user)
-            
-        elif sys.argv[1] == 'rm':
-            uid = input('Username? ')
-            u = testate.User(uid)
-            u.delete()
-
-        else:
-            print(f'unsupported command')
+        run(sys.argv[1])
 
 if __name__ == '__main__':
     main()
