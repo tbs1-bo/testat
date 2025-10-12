ce138756e6e8919a8dd64b9b9403ca939df1cdc5
Marco Bakera <marco@bakera.de>
2025-10-12 22:21:37 +0200

    fix: update poetry command options to use --without=dev instead of --no-dev

--- server_update.sh
+++ server_update.sh
@@ -6,10 +6,10 @@ echo "update repo at server $DEPLOY_SERVER"
 ssh $DEPLOY_SERVER "git -C $DEPLOY_DIR pull"
 
 echo "outdated deps:"
-ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry show --no-dev --outdated"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry show --without=dev --outdated"
 
 echo "update deps:"
-ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update --no-dev"
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && ~/.local/bin/poetry update --without=dev"
 
 echo "restart service"
 ssh -t $DEPLOY_SERVER 'sudo supervisorctl restart testat'
